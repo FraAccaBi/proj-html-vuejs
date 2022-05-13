822690fbb1dc034551595996a84de083d75bddb7
FraAccaBi <francescahuda@gmail.com>
2022-05-13 12:13:17 +0200

    add header structure commit

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,19 +1,59 @@
 <template>
-  <div>
-    
+  <div class="header">
+    <div class="logo"></div>
+    <div class="navbar">
+      <ul>
+        <li v-for="element in navbar" :key="element.id"></li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'SiteHeader',
-  props: {
-   
+  data() {
+   return {
+      navbar: [
+        {
+          name:'Home',
+          id: 1
+        },
+        {
+          name: 'Courses',
+          id: 2
+        },
+        {
+          name:'About Us',
+          id: 3
+        }, 
+        {
+          name:'News',
+          id: 4
+        }, 
+        {
+          name:'Pages',
+          id: 5
+        }, 
+        {
+          name:'Contact',
+          id: 6
+        }, 
+        {
+          name: 'Purchase',
+          id: 7
+        }
+      ]
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
+@import "~@/assets/scss/variables.scss";
+.header{
+  background-color: $bg-pink;
+  height: 100px;
+}
 </style>
