186ec7291af9ca5c68b4654dc9072df21ac4c116
FraAccaBi <francescahuda@gmail.com>
2022-05-13 12:43:48 +0200

    backup commit

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="header">
-    <div class="logo"></div>
-    <div class="navbar">
-      <ul>
-        <li v-for="element in navbar" :key="element.id"></li>
-      </ul>
+  <div class="slider">
+    <div class="header d-flex">
+      <div class="logo">
+        <img class="p-4" src="../../public/img/theme_eduprime_logo.png" alt="">
+      </div>
+      <div class="navbar d-flex ">
+        <ul class="list-unstyled d-flex ">
+          <li v-for="element in navbar" :key="element.id"> {{element.name}}</li>
+        </ul>
+      </div>
+    </div>
+    <div class="main">
+
     </div>
   </div>
+  
 </template>
 
 <script>
@@ -43,7 +51,26 @@ export default {
           name: 'Purchase',
           id: 7
         }
+      ],
+      bg_imgs:[
+        {
+          id:1,
+          url: '../../public/img/theme_slider1_bg-1.jpg'
+        },
+        {
+          id:2,
+          url: '../../public/img/theme_slider2_bg-1.jpg'
+        },
+        {
+          id:3,
+          url: '../../public/img/theme_slider3_bg-1.jpg'
+        }
       ]
+    }
+  },
+  methods: {
+    slider(){
+      setInterval
     }
   }
 }
@@ -52,8 +79,22 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 @import "~@/assets/scss/variables.scss";
-.header{
-  background-color: $bg-pink;
-  height: 100px;
+.slider{
+  background-image: url("../../public/img/theme_slider2_bg-1.jpg");
+  background-position: center;
+  height: 500px;
+
+  .header{
+  background-color: $bg-pink-transparent;
+  height: calc(500px / 5);
+    .logo>img{
+    max-height: 100px;
+    }
+  };
+  .main{
+    height: calc((500px / 5) * 4);
+    background-color: $bg-pink-transparent;
+  }
 }
+
 </style>
